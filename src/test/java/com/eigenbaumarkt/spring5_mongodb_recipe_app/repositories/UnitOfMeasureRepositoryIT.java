905e6f068f36f955123b604073745bcda231cf1f
Mesqualito <jochen@gebsattel.rocks>
2020-09-09 16:04:12 +0200

    Improved Integration Tests in combination with mongodb
    Alternative zum manuellen Leeren der mongodb-Repositories im Test mit spezieller Test-Konfiguration, um das Bootstrap-Package zu laden. Diese Lösung wäre ebenso gut: "Six of one and half a dozen of the other" !
    
    analog zu 'https://www.udemy.com/course/spring-framework-5-beginner-to-guru/learn/lecture/7783314'

--- src/test/java/com/eigenbaumarkt/spring5_mongodb_recipe_app/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/eigenbaumarkt/spring5_mongodb_recipe_app/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,28 +1,46 @@
 package com.eigenbaumarkt.spring5_mongodb_recipe_app.repositories;
 
+import com.eigenbaumarkt.spring5_mongodb_recipe_app.bootstrap.RecipeBootstrap;
 import com.eigenbaumarkt.spring5_mongodb_recipe_app.domain.UnitOfMeasure;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
-@Ignore
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataMongoTest
 public class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
     @Before
+    // we manually have to set up test environment for use with mongodb, exactly: the repositories,
+    // which had been populated by Spring automatically with '@DataJpaTest'
     public void setUp() throws Exception {
+        // because @Before runs on every test, there would be more than one unique value at the second (...) test
+        // if we don't clean up the repositories before initializing them.
+        // With DataJpa, we had transactions and a rollback after every test-method instead (default behaviour in Spring).
+        recipeRepository.deleteAll();
+        unitOfMeasureRepository.deleteAll();
+        categoryRepository.deleteAll();
+
+
+        // now fill the repositories with Bootstrap-data
+        RecipeBootstrap recipeBootstrap = new RecipeBootstrap(categoryRepository, recipeRepository, unitOfMeasureRepository);
+        recipeBootstrap.onApplicationEvent(null);
     }
 
     @Test
